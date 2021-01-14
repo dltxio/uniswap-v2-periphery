df82a8e7c24bdd89d0d494a451201fa2e767c722
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-01-15 09:25:56 +1000

    Update factory

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -4,7 +4,7 @@ module.exports = async (deployer, network, accounts) => {
   let factory = '0x00'
 
   if (network === 'kovan') {
-    factory = '0xf3F8185656b9108Ba532E8Fa69563e5f41cC2403' // Kovan
+    factory = '0x96485389Ff0Ff6048f17BBaCce1E38074ee06838' // Kovan
   }
 
   if (network === 'development') {
@@ -18,4 +18,18 @@ module.exports = async (deployer, network, accounts) => {
   //const weth = '0x0a180A76e4466bF68A7F86fB029BEd3cCcFaAac5'
 
   await deployer.deploy(HandleRouter, factory)
+
+  // test liquidity
+  if (network === 'kovan') {
+    const router = await HandleRouter.deployed();
+
+    const aud = '0xabaf0c048Cb12d8D8f253e861ACe9d3303044F88';
+    const jpy = '0x2457D44852CD8A271a26682CA3C3eC1159c9189E';
+
+    const pair = await router.pairFor(aud, jpy);
+    console.log(pair);
+
+    const reserves = await router.getReserves(aud, jpy);
+    console.log(reserves);
+  }
 }
